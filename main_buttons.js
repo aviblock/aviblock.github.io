09c264e30d31eb0e5eff0f42d32d5ca3468fecc5
aviblock <31028488+aviblock@users.noreply.github.com>
2023-03-14 02:04:32 +0000

    Update

--- main_buttons.js
+++ main_buttons.js
@@ -47,6 +47,18 @@ class TopButton {
 
     callback() {
         var btns = document.getElementsByClassName("btn");
+        // let projs = document.getElementsByClassName("proj_btn");
+        // if (projs.length > 0){
+        //     projs.forEach(element => {
+        //         element.remove();
+        //     });
+        // }
+        try {
+            document.querySelector('#academic').remove();
+            document.querySelector('#professional').remove();
+            document.querySelector('#personal').remove();
+        } catch (error) {}
+        hide_contents();
         if (this.ba.clicked == null) {
             console.log('first time');
             slide_left(btns, this);
@@ -101,11 +113,11 @@ class TopButton {
             duration: 1500
         })
     }
+}
 
-    hide_contents() {
-        let content_box = document.getElementById("content");
-        content_box.innerHTML = "";
-    }
+function hide_contents() {
+    let content_box = document.getElementById("content");
+    if (content_box != null) { content_box.innerHTML = ""; }
 }
 
 function proj_click(cur_btn) {
@@ -133,6 +145,9 @@ function projects() {
     professional_btn.setAttribute("id", "professional");
     content_box.appendChild(professional_btn);
     professional_btn.style.left = '23%';
+    if (isMobile) {
+        professional_btn.style.top = '0%';
+    }
     professional_btn.value = "Professional";
     professional_btn.addEventListener('click', function(){
         proj_click(professional_btn);
@@ -144,6 +159,9 @@ function projects() {
     academic_btn.setAttribute("id", "academic");
     content_box.appendChild(academic_btn);
     academic_btn.style.left = '43%';
+    if (isMobile) {
+        academic_btn.style.top = '0%';
+    }
     academic_btn.value = "Academic";
     academic_btn.addEventListener('click', function(){
         proj_click(academic_btn);
@@ -156,6 +174,9 @@ function projects() {
     personal_btn.setAttribute("id", "personal");
     content_box.appendChild(personal_btn);
     personal_btn.style.left = '63%';
+    if (isMobile) {
+        personal_btn.style.top = '0%';
+    }
     personal_btn.value = "Personal";
     personal_btn.addEventListener('click', function(){
         proj_click(personal_btn);
@@ -202,13 +223,13 @@ function anim_shapes() {
     }
 }
 
-function removeItemOnce(arr, value) {
-    var index = arr.indexOf(value);
-    if (index > -1) {
-      arr.splice(index, 1);
-    }
-    return arr;
-  }
+// function removeItemOnce(arr, value) {
+//     var index = arr.indexOf(value);
+//     if (index > -1) {
+//       arr.splice(index, 1);
+//     }
+//     return arr;
+//   }
 
 function shapes() {
     var pos_shapes = ["line1.png", "line2.png", "line3.png", "line4.png", "line5.png",
@@ -254,7 +275,6 @@ function to_unclicked(elem) {
         width: '40px',
         easing: 'easeInOutQuad'
     })
-    elem.hide_contents();
 }
 
 function slide_left(elems, elem) {
@@ -332,6 +352,8 @@ function reset() {
     console.log('reset');
 }
 
+var isMobile
+
 function setup() {
     var width = window.innerWidth
     || document.documentElement.clientWidth
@@ -341,7 +363,7 @@ function setup() {
     || document.documentElement.clientHeight
     || document.body.clientHeight;
         
-    var isMobile = false; //initiate as false
+    isMobile = false; //initiate as false
     // device detection
     if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent) 
         || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(navigator.userAgent.substr(0,4))) { 
@@ -353,7 +375,7 @@ function setup() {
     var btn3 = new TopButton("Projects", "projects", "52%", isMobile);
     var btn4 = new TopButton("Contact Me", "contact", "67%", isMobile);
     document.getElementById("name").addEventListener("click", reset);
-    shapes();
+    if (!isMobile) { shapes() };
 }
 
 window.onload = setup;
